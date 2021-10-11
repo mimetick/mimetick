2d3f0ad0cc11ca4c5c3fe1622219cddde03663de
Thomas Bailly <3940727+Authfix@users.noreply.github.com>
2021-10-11 20:58:42 +0200

    chore: create default application

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=mimetick_mimetick&metric=alert_status)](https://sonarcloud.io/dashboard?id=mimetick_mimetick)
+
+[![Build](https://github.com/mimetick/mimetick/actions/workflows/build.yml/badge.svg)](https://github.com/mimetick/mimetick/actions/workflows/build.yml)
---
+++ tests/WinApp.Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace Mimetick.WinApp.Tests
+{
+    public class STATestMethodAttribute : TestMethodAttribute
+    {
+        public override TestResult[] Execute(ITestMethod testMethod)
+        {
+            if (Thread.CurrentThread.GetApartmentState() == ApartmentState.STA)
+                return Invoke(testMethod);
+
+            TestResult[] result = null;
+            var thread = new Thread(() => result = Invoke(testMethod));
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+            return result;
+        }
+
+        private TestResult[] Invoke(ITestMethod testMethod)
+        {
+            return new[] { testMethod.Invoke(null) };
+        }
+    }
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [STATestMethod]
+        public void TestMethod1()
+        {
+            var mainWindow = new MainWindow();
+
+            Assert.IsNotNull(mainWindow);
+        }
+    }
+}
