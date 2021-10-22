4242d95f2b93163191c4e4e5dc5a5fd3ece03384
Thomas Bailly <3940727+Authfix@users.noreply.github.com>
2021-10-22 23:36:26 +0200

    chore: add tests to application

---
+++ tests/WinApp.Tests/AppTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Mimetick.Module.Git;
+using Mimetick.Module.Ssh;
+using Mimetick.WinApp.Tests.Utils;
+
+namespace Mimetick.WinApp.Tests
+{
+    [TestClass]
+    public class AppTests
+    {
+        [STATestMethod]
+        public void Should_HaveSshModuleRegister_When_BootstrapApplication()
+        {
+            var app = new App();
+
+            app.Startup += (s, e) => { app.Shutdown(); };
+            app.Run();
+
+            var provider = app.Container.CreateScope();
+            var module = provider.Resolve(typeof(SshModule));
+
+            module.Should().NotBeNull();
+        }
+
+        [STATestMethod]
+        public void Should_HaveGitModuleRegister_When_BootstrapApplication()
+        {
+            var app = new App();
+
+            app.Startup += (s, e) => { app.Shutdown(); };
+            app.Run();
+
+            var provider = app.Container.CreateScope();
+            var module = provider.Resolve(typeof(GitModule));
+
+            module.Should().NotBeNull();
+        }
+    }
+}
