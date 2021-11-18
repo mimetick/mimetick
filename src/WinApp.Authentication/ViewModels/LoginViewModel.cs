9a472e00809826b15610cb87c6872afbbcfd080d
Thomas Bailly <3940727+Authfix@users.noreply.github.com>
2021-11-19 00:18:16 +0100

    feat(auth): add design and navigation

--- src/WinApp.Authentication/ViewModels/LoginViewModel.cs
+++ src/WinApp.Authentication/ViewModels/LoginViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Commands;
+﻿using Mimetick.Core;
+using Prism.Commands;
+using Prism.Regions;
 using System.Windows.Input;
 
 namespace Mimetick.WinApp.Authentication.ViewModels
@@ -6,12 +8,18 @@ namespace Mimetick.WinApp.Authentication.ViewModels
     internal class LoginViewModel
     {
         private readonly AuthenticationService _authenticationService;
+        private readonly IRegionManager _regionManager;
 
-        public LoginViewModel(AuthenticationService authenticationService)
+        public LoginViewModel(AuthenticationService authenticationService, IRegionManager regionManager)
         {
+            _regionManager = regionManager;
             _authenticationService = authenticationService;
 
-            _loginCommand = new DelegateCommand(() => _authenticationService.Authenticate());
+            _loginCommand = new DelegateCommand(async () =>
+            {
+                await _authenticationService.Authenticate();
+                _regionManager.RequestNavigate(RegionNames.MainRegion, "PluginsView");
+            });
         }
 
         private readonly DelegateCommand _loginCommand;
