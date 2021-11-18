ceb06ab451ced23970fea0ec69f42703da7ab292
Thomas Bailly <3940727+Authfix@users.noreply.github.com>
2021-11-18 16:43:27 +0100

    feat(app): add configuration

---
+++ src/Core/AppConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace Mimetick.Core
+{
+    public class AppConfiguration
+    {
+        /// <summary>
+        /// Gets the client identifier
+        /// </summary>
+        public string ClientId { get; private set; }
+
+        /// <summary>
+        /// Gets the tenant id for authentication
+        /// </summary>
+        public string TenantId { get; private set; }
+    }
+}
