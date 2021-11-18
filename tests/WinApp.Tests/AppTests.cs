4de69545d23d03bb72048622357f766465b05894
Thomas Bailly <3940727+Authfix@users.noreply.github.com>
2021-11-19 00:27:13 +0100

    feat(auth): add Microsoft authentication (#18)
    +semver: feature

--- tests/WinApp.Tests/AppTests.cs
+++ tests/WinApp.Tests/AppTests.cs
@@ -5,6 +5,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Mimetick.Core;
 using Mimetick.Plugins.Git;
 using Mimetick.Plugins.Ssh;
+using Mimetick.WinApp.Authentication;
 using Mimetick.WinApp.Tests.Utils;
 
 using System;
@@ -26,6 +27,12 @@ namespace Mimetick.WinApp.Tests
             ValidateAppModule(typeof(GitModule), "Git");
         }
 
+        [STATestMethod]
+        public void Should_HaveAuthenticationModuleRegister_When_BootstrapApplication()
+        {
+            ValidateAppModule(typeof(AuthenticationModule), null);
+        }
+
         private void ValidateAppModule(Type moduleToValidate, string name)
         {
             var app = ApplicationTest.Instance;
@@ -33,9 +40,15 @@ namespace Mimetick.WinApp.Tests
             var provider = app.Container.CreateScope();
 
             var module = provider.Resolve(moduleToValidate);
-            var plugin = provider.Resolve(typeof(IMimetickPlugin), name) as IMimetickPlugin;
 
             module.Should().NotBeNull();
+
+            if (string.IsNullOrEmpty(name))
+            {
+                return;
+            }
+
+            var plugin = provider.Resolve(typeof(IMimetickPlugin), name) as IMimetickPlugin;
 
             plugin.Should().NotBeNull();
             plugin.Name.Should().NotBeEmpty();
